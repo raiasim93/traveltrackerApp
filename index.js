0ec6b6e72ea748284ba42b284e66bc8386edae0a
raiasim93 <asimrai89@gmail.com>
2024-02-10 16:18:07 +1100

    update the visited countries in the frontend when the database gets updated

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
 // importing modules 
 import express from "express";
-import bodyParser from "bodyParser";
+import bodyParser from "body-parser";
 import pg from "pg";
 
 // setting port and starting express server
 const port = 3000;
 const app = express();
 
+// setting up pg database 
+const db = new pg.Client({
+    user: "postgres",
+    host: "localhost",
+    database: "world",
+    password: "2012",
+    port: 5432
+});
+
+db.connect();
 
 
+// setting up home route 
+app.get("/", async(req, res)=> {
+    const result = await db.query("SELECT country_code FROM visited_countries");
+    let countries = [ ];
+    result.rows.forEach((country) => {
+        countries.push(country.country_code);
+    });
+    console.log(result.rows);
+    res.render("index.ejs", {
+        countries: countries,
+        total: countries.length
+    });
+})
 
 //using middleware
 app.use(bodyParser.urlencoded({extended: true}));
