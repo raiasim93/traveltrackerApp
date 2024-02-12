6dbc03597737371d5bbca563a4b2c11011ea68f1
raiasim93 <asimrai89@gmail.com>
2024-02-13 09:06:31 +1100

    allow tracker to update when user enters the country name

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ import pg from "pg";
 const port = 3000;
 const app = express();
 
+//using middleware
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public')); 
+
 // setting up pg database 
 const db = new pg.Client({
     user: "postgres",
@@ -33,9 +37,24 @@ app.get("/", async(req, res)=> {
     });
 })
 
-//using middleware
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static('public')); 
+// setting up the post route or create a new country
+app.post("/add", async (req, res)=> {
+    const input = req.body["country"];
+
+    const result = await db.query("SELECT country_code FROM countries WHERE country_name = $1", 
+    [input]);
+
+    if(result.rows.length !== 0){
+        const data = result.rows[0];
+        const country_code = data.country_code;
+
+        await db.query("INSERT into visited_countries (country_code) VALUES ($1)", 
+        [country_code]);
+        res.redirect("/");
+    }
+    
+});
+
 
 // express server listening 
 app.listen(port, ()=> {
